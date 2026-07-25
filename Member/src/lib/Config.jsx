5935bcef9c771512c00dad5829e14158e181fccb
Nakul <nickshah403@gmail.com>
2026-07-25 13:13:57 +0530

    remove member pannel

--- Member/src/lib/Config.jsx
+++
@@ -1,6 +0,0 @@
-const backendUrl = import.meta.env.VITE_API_URL || "https://uttkarsh-backend.onrender.com";
-
-export const Config = {
-  API_HOST_URl: `${backendUrl}/api/`,
-  API_URL: `${backendUrl}/`,
-};
